895ed886d3edad28d29dc08352cf6b7d890e5884
grantbrown-adl <grant.brown@outlook.com>
2022-09-15 11:26:52 +0930

    Proper Renderer

--- kinfu-point-cloud/Assets/PointCloudRenderer.cs
+++ kinfu-point-cloud/Assets/PointCloudRenderer.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
+using System.IO;
 
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class PointCloudRenderer : MonoBehaviour
 {
     Texture2D texColor;
@@ -17,46 +19,43 @@ public class PointCloudRenderer : MonoBehaviour
     public Vector3 boundSize;
     public Vector3 boundCentre;
 
-    public Mesh mesh;
-
-    public bool xDif, yDif, zDif;
-
+    Mesh mesh;
     Vector3[] vertices;
     Color[] colors;
+    
+    // public Mesh procMesh;
+
+    List<Vector3> pointVertices= new List<Vector3>();
+    List<Vector3> pointNormals = new List<Vector3>();
+
+    void OnEnable(){
+        mesh = new Mesh{ name = "procedural_mesh" };
+        // mesh.vertices = new Vector3[]{
+        //     Vector3.zero, Vector3.right, Vector3.up
+        // };
+        GetComponent<MeshFilter>().mesh = mesh; 
+        // mesh.triangles = new int[] {0,2,1};
+    }
 
     private void Start()
     {
         vfx = GetComponent<VisualEffect>();
 
-        // Vector3[] positions = new Vector3[(int)resolution * (int)resolution];
         colors = new Color[(int)resolution * (int)resolution];
-        vertices = mesh.vertices;
-
-        for (int x = 0; x < (int)resolution; x++)
-        {
-            for (int y = 0; y < (int)resolution; y++)
-            {
-                float distance = x / 500;
-                // colors[x + y * (int)resolution] = new Color(vertices[x].x, vertices[y].y, vertices[x].z);
-                // positions[x + y * (int)resolution] = new Vector3(Random.value * 10, Random.value * 10, Random.value * 10);
-                colors[x + y * (int)resolution] = new Color(Random.value, Random.value, Random.value, 1);
-
-                // colors[x + y * (int)resolution] = new Color(1, 1, 1, 1);
-                //  colors[x + y * (int)resolution] = new Color()
-            }
-        }
-        // SetParticles(positions, colors);
-
-        SetParticles(vertices, colors);
+        // Use mesh rather than list of points
+        // vertices = mesh.vertices;
+        // SetParticles(vertices, colors);
+        // Get file and convert to point and normal array
+        string[] vertexPoints = ReadFile();
+        // Use point list to create vector3 points
+        SetVertices(vertexPoints);  
+        // After we've assigned points we can build the cloud
+        SetParticles(pointVertices, colors);
+        
     }
 
     private void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            // SetParticles(vertices, colors);
-        }
-
         if (toUpdate)
         {
             toUpdate = false;
@@ -71,16 +70,14 @@ public class PointCloudRenderer : MonoBehaviour
         }
     }
 
-    public void SetParticles(Vector3[] positions, Color[] colors)
+    // public void SetParticles(Vector3[] positions, Color[] colors)
+    public void SetParticles(List<Vector3> positions, Color[] colors)
     {
-        texColor = new Texture2D(positions.Length > (int)resolution ? (int)resolution : positions.Length, Mathf.Clamp(positions.Length / (int)resolution, 1, (int)resolution), TextureFormat.RGBAFloat, false);
-        texPosScale = new Texture2D(positions.Length > (int)resolution ? (int)resolution : positions.Length, Mathf.Clamp(positions.Length / (int)resolution, 1, (int)resolution), TextureFormat.RGBAFloat, false);
+        texColor = new Texture2D(positions.Count > (int)resolution ? (int)resolution : positions.Count, Mathf.Clamp(positions.Count / (int)resolution, 1, (int)resolution), TextureFormat.RGBAFloat, false);
+        texPosScale = new Texture2D(positions.Count > (int)resolution ? (int)resolution : positions.Count, Mathf.Clamp(positions.Count / (int)resolution, 1, (int)resolution), TextureFormat.RGBAFloat, false);
 
         int texWidth = texColor.width;
         int texHeight = texColor.height;
-
-        // Vector3[] reversi = positions;
-        // System.Array.Reverse(reversi);
 
         for (int y = 0; y < texHeight; y++)
         {
@@ -88,52 +85,83 @@ public class PointCloudRenderer : MonoBehaviour
             {
                 int index = x + y * texWidth;
 
-                // texColor.SetPixel(x, y, colors[index]);
+                // 3 is an arbitrary number, if we can get the furthest positions[index].z from origin (0,0,0)
+                // we should use that
+                float percentageDistance = decimalPercent(positions[index].z, 0, 3);
 
-                // Vector3 difference = new Vector3(Vector3.zero.x - positions[index].x, Vector3.zero.y - positions[index].y, Vector3.zero.z - positions[index].z);
-                // float divisorZ = (difference.z / 15) + 0.05f;
-                // float divisorX = (difference.x / 15) + 0.05f;
-                // float divisorY = (difference.y / 15) + 0.05f;
+                float difference = (percentageDistance - 1) * -1;
 
-                // Vector3 values = new Vector3(positions[index].x, positions[index].y, positions[index].z);
-                // divisorZ = (values.z / 15) + 0.05f;
-                // divisorX = (values.x / 15) + 0.05f;
-                // divisorY = (values.y / 15) + 0.05f;
+                texColor.SetPixel(x, y, new Color(difference, difference, difference, 1));
 
-
-                // Vector3 values = new Vector3(positions[index].x, positions[index].y, positions[index].z);
-                float minZ = remap(positions[index].z, 0, 3);
-                // Debug.Log(minZ);
-
-                // float divisorZ = (positions[index].z / 10) + 0.05f;
-                // float divisorX = (positions[index].x / 10) + 0.05f;
-                // float divisorY = (positions[index].y / 10) + 0.05f;
-                float divisorZ = (minZ - 1) * -1;
-                float divisorX = (positions[index].x / 10) + 0.05f;
-                float divisorY = (positions[index].y / 10) + 0.05f;
-
-                if (xDif) texColor.SetPixel(x, y, new Color(divisorX, divisorX, divisorX, 1));
-                else if (yDif) texColor.SetPixel(x, y, new Color(divisorY, divisorY, divisorY, 1));
-                else if (zDif) texColor.SetPixel(x, y, new Color(divisorZ, divisorZ, divisorZ, 1));
-                else texColor.SetPixel(x, y, new Color(divisorZ, divisorZ, divisorZ, 1));
-
-
-                // var data = new Color(255, 255, 255, 1);
-                // var data = new Color(positions[index].x, positions[index].y, positions[index].z, particleSize);
                 var data = new Color(positions[index].x, positions[index].y, positions[index].z, particleSize);
                 texPosScale.SetPixel(x, y, data);
+
             }
         }
 
         texColor.Apply();
         texPosScale.Apply();
-        particleCount = (uint)positions.Length;
+        particleCount = (uint)positions.Count;
         toUpdate = true;
     }
 
-
-    float remap(float val, float min, float max)
+    /// Set a decimal percentage (1 = 100%, 0.5 = 50% etc.) of passed value between min/max
+    /// Used as we are using HDRP which uses 0.0 - 1.0 for RGB values and anything >1.0 
+    /// sets intensity
+    float decimalPercent(float val, float min, float max)
     {
-        return (val - min) / (max - min) * (1 - 0);
+
+        return (val - min) / (max - min);
+    }
+
+    string ReadFile(){
+        // File to read in and pass to our streamReader
+        string filePath = "Assets/kf_output.ply";
+        StreamReader streamReader = new StreamReader(filePath);
+        string pointFile = streamReader.ReadToEnd();
+        // Split into array to seperate header and point data
+        string[] headerRemoved = pointFile.Split("end_header");
+        // Stringify the first element which contains point and normal data
+        string dataString = headerRemoved[1];
+        // Split based on new lines
+        // string[] seperateLines = dataString.Split("\n");
+        streamReader.Close();
+        return dataString.Split("\n");
+        // GilgaMesh(pointVertices);
+    }
+
+    void SetVertices(string[] points){
+        //First and last lines are empty so we start i at 1 and go to length-1
+        for (int i = 1; i < points.Length - 1; i++)
+        {
+            // Split based on space
+            string[] buildIt = points[i].Split(" ");
+            // First three elements are x, y, z points
+            float pointX = float.Parse(buildIt[0]);
+            float pointY = float.Parse(buildIt[1]);
+            float pointZ = float.Parse(buildIt[2]);
+            // which we map to a new vector and add to our list
+            pointVertices.Add(new Vector3(pointX, pointY, pointZ));
+            // Implement normals? currently some are getting passed as nan's
+            // this causes the below to error. 
+            // float nX = float.Parse(buildIt[3]);
+            // float nY = float.Parse(buildIt[4]);
+            // float nZ = float.Parse(buildIt[5]);
+            // pointNormals.Add(new Vector3(nX,nY,nZ));
+        }
+    }
+
+    void GilgaMesh(List<Vector3> points){
+        Vector3[] array = points.ToArray();
+        // Debug.Log(array);
+
+        // var mesh = new Mesh{ name = "procedural_mesh" };
+        mesh.vertices = array;
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        // GetComponent<MeshFilter>().mesh = mesh; 
+        mesh.triangles = new int[] {0,2,1};
     }
 }
